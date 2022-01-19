eafacb3b2552d1e6ecf054c858ae3283f274d09b
devansh-dvj <devansh.dvj@gmail.com>
2022-01-19 19:32:17 +0530

    Added data using cur.execute to insert script

--- insert.py
+++ insert.py
@@ -1,6 +1,17 @@
 import argparse
+import csv
 
 import psycopg2 as pg
+
+files = ['player.csv',
+         'umpire.csv',
+         'team.csv',
+         'owner.csv',
+         'venue.csv',
+         'match.csv',
+         'player_match.csv',
+         'umpire_match.csv',
+         'ball_by_ball.csv']
 
 
 def ddl(cur, ddl_file):
@@ -11,6 +22,21 @@ def ddl(cur, ddl_file):
         cur.execute(crt)
     except Exception as error:
         print(error)
+
+
+def data(cur, data_folder):
+    for file_name in files:
+        with open(data_folder + file_name, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                try:
+                    cur.execute('INSERT INTO {} VALUES {}'.format(
+                        file_name.split('.')[0],
+                        str(tuple(row))))
+                except Exception as error:
+                    print(error)
 
 
 if __name__ == '__main__':
@@ -29,6 +55,7 @@ if __name__ == '__main__':
     cur = conn.cursor()
 
     ddl(cur, args.ddl)
+    data(cur, args.data)
     conn.commit()
 
     cur.close()
